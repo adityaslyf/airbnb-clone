cb071ab0ce9ffbcd5798233d541cc249f61b703c
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-09-10 00:52:31 +0530

    show jokes using props

--- AirbnbClone/src/App.jsx
+++ AirbnbClone/src/App.jsx
@@ -14,8 +14,11 @@ export default function App() {
     return (
         <div>
             <Jokes
-                punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-            />
+                        
+              punchline="ddm" />
+
+              {/* comments = {[{"2","3"}]}
+              integer = {0} */}
             <Jokes
                 setup="I got my daughter a fridge for her birthday." 
                 punchline="I can't wait to see her face light up when she opens it." 
@@ -42,3 +45,44 @@ export default function App() {
        
   
 
+// import React from "react"
+// import Joke from "./Joke"
+
+// /*
+// Challenge: turn the strings in the array into <h3> elements instead
+// */
+
+// export default function App() {
+//     const colors = [
+//             <h3>Red</h3>, 
+//             <h3>Orange</h3>, 
+//             <h3>Yellow</h3>,
+//             <h3>Green</h3>,
+//             <h3>Blue</h3>,
+//             <h3>Indigo</h3>,
+//             <h3>Violet</h3>
+//         ]
+//     return (
+//         <div>
+//             {colors}
+//         </div>
+//     )
+// }
+
+
+
+
+// import React from "react"
+// import Joke from "./Joke"
+// import jokesData from "./jokesData"
+
+// export default function App() {
+//     const jokeElements = jokesData.map(joke => {
+//         return <Joke setup={joke.setup} punchline={joke.punchline} />
+//     })
+//     return (
+//         <div>
+//             {jokeElements}
+//         </div>
+//     )
+// }
---
+++ AirbnbClone/src/components/map.js
@@ -0,0 +1,21 @@
+
+//  E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]*/ const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]of the original strings in an HTML-like <p></p> tag.Given an array of strings, return an array of strings that wraps eachChallenge 3:/*   // console.log(capitalized))// Your code hereconst capitalized = names.map(name => (    name[0].toUpperCase() + name.slice(1))const names = ["alice", "bob", "charlie", "danielle"]// -->        ["Alice", "Bob", "Charlie", "Danielle"] // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]// Your code here const paragraphs = pokemon.map(mon => `<p>${mon}</p>`) console.log(paragraphs)
+// const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// // -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// // Your code here
+
+// const paragraphs = pokemon.map(mon => `<p>${mon}</p>`)
+
+// console.log(paragraphs)
+// Find
+// No Results
+
+
+// CONSOLE
+// ›["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// /index.html
+
+// ...
+// ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+
+// Accepting 'pokemon' did insert the following text: pokemon
